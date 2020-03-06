a180af7ec8aeeaaefedcb175715a4668a7a6f315
sosdanvk <61888716+sosdanvk@users.noreply.github.com>
2020-03-07 02:31:45 +0300

    Delete en.js

--- exit/en.js
+++
@@ -1,9 +0,0 @@
-var p = [
-    "YUKI. N>","If you are reading this, I am probably no longer myself.",
-    "YUKI. N>","When this message appears, it means that you, me, Suzumiya Haruhi, Asahina Mikuru and Koizumi Itsuki have all been gathered here.",
-    "YUKI. N>","This is the key. \nYou have found the answer.",
-    "YUKI. N>","This is the Emergency Escape Program.\n\nTo activate it, hit the “Enter” key, otherwise, hit any other key.\n\nOnce activated, you will be given a chance to repair the space-time continuum. However, neither success nor your safe return can be guaranteed.",
-    "YUKI. N>","This program can only be executed once. Once it is executed, it will be deactivated.\n\nShould you choose not to activate it, it will also be deactivated.\n",
-    " ","Ready?"
-]; 
-var speed = 80;
